3d36d6eaa3204b235d4acfc13606556ba02d10e4
lolkt <969070578@qq.com>
2022-01-26 16:22:37 +0800

    init 222

--- ruoyi-fdfs/src/main/java/com/ruoyi/RuoYiFDfsApp.java
+++ ruoyi-fdfs/src/main/java/com/ruoyi/RuoYiFDfsApp.java
@@ -3,12 +3,16 @@ package com.ruoyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class RuoYiFDfsApp
-{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(RuoYiFDfsApp.class, args);
+public class RuoYiFDfsApp {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(RuoYiFDfsApp.class, args);
+
+        Environment environment = context.getBean(Environment.class);
+
+        System.out.println("访问链接：http://localhost:" + environment.getProperty("local.server.port"));
     }
 }
